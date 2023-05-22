a593dbb9acc6514cac3a1184351826023dd4e7bd
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2023-05-21 23:50:27 -0300

    Added env lib

--- src/main/kotlin/bot/KGnomeRunner.kt
+++ src/main/kotlin/bot/KGnomeRunner.kt
@@ -13,14 +13,15 @@ import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.kordLogger
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
+import io.github.cdimascio.dotenv.dotenv
+
 
 private lateinit var kordInstance: Kord
 
 class KGnomeRunner
 suspend fun main(vararg args: String) {
-
     kordInstance = Kord(token = args.firstOrNull()
-        ?: System.getenv(DISCORD_TOKEN_ENV_VAR_NAME)
+        ?: dotenv()[DISCORD_TOKEN_ENV_VAR_NAME]
         ?: error("token required"))
 
     kordInstance.onIgnoringBots<MessageCreateEvent> {
