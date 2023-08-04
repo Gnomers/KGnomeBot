f0cf88bf36d32376d8e1107cb41a11a349098597
Hozerino <hozerboy2239@gmail.com>
2023-08-03 22:34:27 -0300

    Fix gnome describe validation

--- src/main/kotlin/bot/command/chat/dynamic/DescribeCommand.kt
+++ src/main/kotlin/bot/command/chat/dynamic/DescribeCommand.kt
@@ -6,15 +6,21 @@ import bot.utilities.isValidURL
 import dev.kord.core.behavior.channel.withTyping
 import dev.kord.core.event.message.MessageCreateEvent
 
+private const val INVALID_IMAGE = "Ho ho ho ha ha, you can't gnome me! Please provide an image or a valid image URL."
+
 class DescribeCommand: Command(
     name = "describe",
     description = "Describes an image",
-    hasSubCommand = true
+    hasSubCommand = false
 ) {
     override suspend fun invoke(event: MessageCreateEvent, subCommand: String?) {
+        val message = event.message
+        val attachment = message.attachments.firstOrNull()
+        if (subCommand == null && attachment == null) {
+            event.message.channel.createMessage(INVALID_IMAGE)
+        }
         event.message.channel.withTyping {
-            val message = event.message
-            val attachment = message.attachments.firstOrNull()
+            val attachment = attachment
             val url = attachment?.let {
                 if (it.isImage) it.url
                 else null
@@ -24,7 +30,7 @@ class DescribeCommand: Command(
                 val description = ImageDescriptorService.describe(url)
                 event.message.channel.createMessage(description)
             } else {
-                event.message.channel.createMessage("Ho ho ho ha ha, you can't gnome me! Please provide an image or a valid image URL.")
+                event.message.channel.createMessage(INVALID_IMAGE)
             }
         }
     }
