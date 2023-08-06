b4a61f3cc0b014d0068600c50a34a59d60a8e519
Hozerino <hozerboy2239@gmail.com>
2023-08-06 20:02:33 -0300

    Faliceu command

---
+++ src/main/kotlin/bot/command/sound/simple/FaliceuCommand.kt
@@ -0,0 +1,15 @@
+package bot.command.sound.simple
+
+import bot.command.Command
+import bot.core.voice.SoundPlayerManager
+import bot.utilities.Sound
+import dev.kord.core.event.message.MessageCreateEvent
+
+class FaliceuCommand: Command(
+    name = "faliceu",
+    description = "Plays a \"Faliceu\""
+) {
+    override suspend fun invoke(event: MessageCreateEvent, subCommand: String?) {
+        SoundPlayerManager.playSoundForMessage(event = event, sound = Sound.FALICEU)
+    }
+}
