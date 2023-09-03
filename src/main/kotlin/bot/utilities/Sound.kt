ec079c610ff3df854a22a98c3a515c3947d65336
Hozerino <hozerboy2239@gmail.com>
2023-09-02 21:55:06 -0300

    [C] Add SHADOW_WIZARD

--- src/main/kotlin/bot/utilities/Sound.kt
+++ src/main/kotlin/bot/utilities/Sound.kt
@@ -23,6 +23,7 @@ enum class Sound(val path: String) {
     RONALDINHO_SOCCER("audio/ronaldinho_soccer.mp3"),
     JOHN_ELDEN_RING("audio/john_elden_ring.mp3"),
     E_O_PIX("audio/e_o_pix.mp3"),
+    SHADOW_WIZARD("audio/shadow_wizard.mp3"),
     USSR_ANTHEM("audio/ussr_anthem.mp3"),
     AMOGUS("audio/amogus.mp3");
 
