35a46c240fe6a63a0b4022185508b8c068e52920
Hozerino <hozerboy2239@gmail.com>
2023-12-20 12:50:51 -0300

    Add sound from They Hunger

---
+++ src/main/kotlin/bot/trigger/chat/SonOfBeitchRegexTrigger.kt
@@ -0,0 +1,25 @@
+package bot.trigger.chat
+
+import bot.core.voice.SoundPlayerManager
+import bot.trigger.Trigger
+import bot.utilities.Sound
+import bot.utilities.isCommand
+import bot.utilities.onIgnoringBots
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+
+object SonOfBeitchRegexTrigger: Trigger(
+    name = "son_of_beitch",
+    description = "Cute speech from a cute character from Half-Life's mod \"They Hunger\""
+) {
+    val regex = Regex(".*fdp.*|.*bitch.*|.*puta.*", setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.IGNORE_CASE))
+
+    override suspend fun register(kordInstance: Kord) {
+        kordInstance.onIgnoringBots<MessageCreateEvent> {
+            val message = this.message.content
+            if (!message.isCommand() && message.matches(regex)) {
+                SoundPlayerManager.playSoundForMessage(this, Sound.SON_OF_BEITCH)
+            }
+        }
+    }
+}
