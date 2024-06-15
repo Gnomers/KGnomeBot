517aa1cb0622bead90993cd4d74e4fcfbfc670ae
Hozerino <hozerboy2239@gmail.com>
2024-06-15 20:00:55 -0300

    add awan-llm and kill chatgpt

--- src/main/kotlin/bot/command/chat/dynamic/ChatCommand.kt
+++ src/main/kotlin/bot/command/chat/dynamic/ChatCommand.kt
@@ -1,18 +1,18 @@
 package bot.command.chat.dynamic
 
 import bot.command.Command
-import bot.core.service.chatgpt.ChatGPTService
+import bot.core.service.awanllm.AwanLLMService
 import dev.kord.core.behavior.channel.withTyping
 import dev.kord.core.event.message.MessageCreateEvent
 
-object GnomeChatCommand: Command(
+object ChatCommand: Command(
     name = "chat",
     description = "Talk with a REAL GNOME - definitely not an AI",
     hasSubCommand = true
 ) {
     override suspend fun invoke(event: MessageCreateEvent, subCommand: String?) {
         event.message.channel.withTyping {
-            event.message.channel.createMessage(ChatGPTService.prompt(subCommand!!, true))
+            event.message.channel.createMessage(AwanLLMService.prompt(subCommand!!))
         }
     }
 }
