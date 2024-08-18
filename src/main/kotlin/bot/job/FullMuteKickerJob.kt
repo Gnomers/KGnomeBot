c69db2cf00443b56a35ffa31138af8b1b206bcfa
Hozerino <hozerboy2239@gmail.com>
2024-08-17 21:09:42 -0300

    [C] Disconnects player for reals

--- src/main/kotlin/bot/job/FullMuteKickerJob.kt
+++ src/main/kotlin/bot/job/FullMuteKickerJob.kt
@@ -1,6 +1,7 @@
 package bot.job
 
 import bot.kordInstance
+import bot.utilities.disconnect
 import bot.utilities.isFullMuted
 import dev.kord.common.entity.Snowflake
 import kotlinx.coroutines.flow.flatMapMerge
@@ -10,11 +11,11 @@ import kotlinx.coroutines.flow.toList
 object FullMuteKickerJob : Job(
     name = "full_mute_kicker",
     description = "Kicks everyone who is fully muted (no mic and no sound)",
-    // 1.5 hour
-    executionDelaySeconds = 2400
+    // 1 hour
+    executionDelaySeconds = 3600
 ) {
 
-    val ROUNDS_BEFORE_KICKING = 3
+    val ROUNDS_BEFORE_KICKING = 1
 
     // Could've used a collection of VoiceStates, but it is not a good class to check for equality.
     // Also it would (PROBABLY) be reset if the user re-entered between the checks
@@ -31,10 +32,10 @@ object FullMuteKickerJob : Job(
                     roundsMuted[it.userId] = roundsMuted.getOrDefault(it.userId, 0)
 
                     when {
-                            roundsMuted[it.userId] == ROUNDS_BEFORE_KICKING -> {
+                        roundsMuted[it.userId] == ROUNDS_BEFORE_KICKING -> {
                             roundsMuted[it.userId] = 0
-                            logger.info("User ${member.username} would have been disconnected after of ${(ROUNDS_BEFORE_KICKING * executionDelaySeconds)/60} minutes being fully muted.")
-//                            member.disconnect()
+                            member.disconnect()
+                            logger.info("User ${member.username} was disconnected after of ${(ROUNDS_BEFORE_KICKING * executionDelaySeconds) / 60} minutes being fully muted.")
                         }
 
                         else -> {
