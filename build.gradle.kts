e4383dce05512e136f70d93e4ce01b947d8bcb23
Hozerino <hozerboy2239@gmail.com>
2024-08-24 19:25:57 -0300

    local lavaplayer libs + http client

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,17 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
 
     // Audio player
-    implementation("com.sedmelluq:lavaplayer:1.3.73")
+//    implementation("com.sedmelluq:lavaplayer:1.3.47")
+    implementation(files("/libs/com.sedmelluq/lavaplayer/1.3.73/414880b4e42365a6d2f014b251edc02c6f073e00/lavaplayer-1.3.73.jar"))
+    implementation(files("/libs/com.sedmelluq/lava-common/1.1.2/c9e2c5192a93847edd6b96c2f93530fdcda85028/lava-common-1.1.2.jar"))
+//    implementation(files("/libs/com.sedmelluq/lavaplayer/1.3.77/89401c843d79108a69319bfc2e9e043335165ce5/lavaplayer-1.3.77.jar"))
 
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 
     // HTTP
     implementation("com.squareup.okhttp:okhttp:2.7.5")
+    implementation("org.apache.httpcomponents:httpclient:4.5")
+
 
     // Command registering
     implementation("org.reflections:reflections:0.10.2")
